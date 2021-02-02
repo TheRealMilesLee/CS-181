811c65b72c31b4749f859bd22370693ba6317c25
Serendipity <1789216340lhy@gmail.com>
2021-02-02 16:19:09 -0600

    Update

--- Lab2-Intro_Class/cmake-build-debug/CMakeFiles/Lab2_Intro_Class.dir/DependInfo.cmake
+++ Lab2-Intro_Class/cmake-build-debug/CMakeFiles/Lab2_Intro_Class.dir/DependInfo.cmake
@@ -4,11 +4,10 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/leemiles/ComputerScienceRelated/Spring2021-CS181/Lab2-Intro_Class/Intro_Class.cpp" "/Users/leemiles/ComputerScienceRelated/Spring2021-CS181/Lab2-Intro_Class/cmake-build-debug/CMakeFiles/Lab2_Intro_Class.dir/Intro_Class.cpp.o"
-  "/Users/leemiles/ComputerScienceRelated/Spring2021-CS181/Lab2-Intro_Class/Student.cpp" "/Users/leemiles/ComputerScienceRelated/Spring2021-CS181/Lab2-Intro_Class/cmake-build-debug/CMakeFiles/Lab2_Intro_Class.dir/Student.cpp.o"
-  "/Users/leemiles/ComputerScienceRelated/Spring2021-CS181/Lab2-Intro_Class/StudentCollection.cpp" "/Users/leemiles/ComputerScienceRelated/Spring2021-CS181/Lab2-Intro_Class/cmake-build-debug/CMakeFiles/Lab2_Intro_Class.dir/StudentCollection.cpp.o"
+  "D:/CS-Related/Spring2021-CS181/Lab2-Intro_Class/Intro_Class.cpp" "D:/CS-Related/Spring2021-CS181/Lab2-Intro_Class/cmake-build-debug/CMakeFiles/Lab2_Intro_Class.dir/Intro_Class.cpp.obj"
+  "D:/CS-Related/Spring2021-CS181/Lab2-Intro_Class/StudentCollection.cpp" "D:/CS-Related/Spring2021-CS181/Lab2-Intro_Class/cmake-build-debug/CMakeFiles/Lab2_Intro_Class.dir/StudentCollection.cpp.obj"
   )
-set(CMAKE_CXX_COMPILER_ID "AppleClang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
--- Lab2-Intro_Class/cmake-build-debug/CMakeFiles/Lab2_Intro_Class.dir/cmake_clean.cmake
+++ Lab2-Intro_Class/cmake-build-debug/CMakeFiles/Lab2_Intro_Class.dir/cmake_clean.cmake
@@ -1,9 +1,10 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Lab2_Intro_Class.dir/Intro_Class.cpp.o"
-  "CMakeFiles/Lab2_Intro_Class.dir/Student.cpp.o"
-  "CMakeFiles/Lab2_Intro_Class.dir/StudentCollection.cpp.o"
-  "Lab2_Intro_Class"
+  "CMakeFiles/Lab2_Intro_Class.dir/Intro_Class.cpp.obj"
+  "CMakeFiles/Lab2_Intro_Class.dir/StudentCollection.cpp.obj"
+  "Lab2_Intro_Class.exe"
+  "Lab2_Intro_Class.exe.manifest"
   "Lab2_Intro_Class.pdb"
+  "libLab2_Intro_Class.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
