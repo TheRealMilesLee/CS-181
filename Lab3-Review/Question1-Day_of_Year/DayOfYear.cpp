34e23d031fce705f0845cf3ccaaf264b3223294e
Serendipity <1789216340lhy@gmail.com>
2021-02-25 19:40:45 -0600

    Update

--- Lab3-Review/Question1-Day_of_Year/DayOfYear.cpp
+++ Lab3-Review/Question1-Day_of_Year/DayOfYear.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <ctime>
 
-const unsigned TOTAL_MONTH_IN_YEAR = 12;
 class DayofYear_Class
 {
 private:
@@ -23,31 +21,19 @@ DayofYear_Class::DayofYear_Class(int day)
 
 void DayofYear_Class::conversion()
 {
-  std::vector<int> days_of_month{31, 60, 91, 121, 152, 182, 213, 244, 273, 304, 334, 365};
+  std::vector<int> days_of_month{0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
   std::vector<std::string> NameOfMonth{"January", "February", "March", "April", "May", "June", "July", "August", "September","October", "November", "December"};
-  if(day_of_year <= days_of_month.at(0))
+  bool done = true;
+  unsigned count = 0;
+  while (done)
   {
-    month = NameOfMonth.at(0);
-    day_in_the_month = day_of_year;
-  }
-  else
-  {
-    int compare_indx_front;
-    int compare_indx_back;
-    for(size_t looptimes = 0; looptimes < days_of_month.size(); looptimes++)
+    if(day_of_year <= days_of_month.at(count + 1))
     {
-        compare_indx_front = days_of_month.at(looptimes);
-        compare_indx_back = days_of_month.at(looptimes++);
-        if(day_of_year > compare_indx_front && day_of_year < compare_indx_back)
-        {
-          month = NameOfMonth.at(looptimes++);
-          day_in_the_month = day_of_year - compare_indx_front;
-        }
-        else
-        {
-          std::cout << "Something goes wrong..";
-        }
+      month = NameOfMonth.at(count);
+      day_in_the_month = day_of_year - days_of_month.at(count--);
+      done = false;
     }
+    count++;
   }
 }
 
@@ -56,8 +42,6 @@ std::string DayofYear_Class::to_string()
   std::string result = month + " " + std::to_string(day_in_the_month);
   return result;
 }
-
-
 
 int main()
 {
