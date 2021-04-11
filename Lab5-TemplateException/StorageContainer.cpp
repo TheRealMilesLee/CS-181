edc297a3e02b968372392725784a210e17e8ef4b
Serendipity <1789216340lhy@gmail.com>
2021-04-10 22:55:48 -0500

    update

--- Lab5-TemplateException/StorageContainer.cpp
+++ Lab5-TemplateException/StorageContainer.cpp
@@ -11,48 +11,41 @@ template <class T>
 class Storage
 {
 private:
-    T *array;
-    int size;
+  T *array;
+  int size;
 public:
-    Storage(int length){size = length; array[size];}
-    Storage(const Storage &elem);
-    ~Storage();
-    int array_size() const{return size;}
-    
-    T &operator[](int index)
+  explicit Storage(int length){size = length; array = new T [size];}
+  Storage(const Storage &elem);
+  ~Storage();
+  int array_size() const{return size;}
+  T &operator[](int index)
+  {
+    try
     {
-      try
+      if (index >= size || index <= 0)
       {
-        if (index >= size || index <= 0)
-        {
-          throw StorageException();
-        }
-        else
-        {
-          return array[index];
-        }
-      }catch(const char &error)
-      {
-        std::cout << "Error: " << error << std::endl;
+        throw StorageException();
       }
+    }catch(const char &error)
+    {
+      std::cout << "Error: " << error << std::endl;
     }
-    void display() const;
-    friend std::ostream &operator<<(std::ostream &os, Storage<T> &elem);
-    friend T maximum(Storage<T> &elem);
-    friend T searchElement(T searchVal, Storage<T> &elem);
+    return array[index];
+  }
+  void display() const;
+  friend std::ostream &operator<<(std::ostream &os, Storage<T> elem);
+  friend T maximum(Storage<T> elem);
+  friend T searchElement(T searchVal, Storage<T> elem);
 };
 
 template<class T>
 Storage<T>::Storage(const Storage &elem)
 {
-  size = elem.array_size();
   // allocate memory
   array = new T [size];
   // copy individual elements from the array
-  for(size_t loop = 0; loop < size; loop++)
-  {
-    *(array + loop) = *(elem.array + loop);
-  }
+  array = elem.array;
+  size = elem.array_size();
 }
 
 template<class T>
@@ -64,17 +57,16 @@ Storage<T>::~Storage()
 }
 
 template<class T>
-std::ostream &operator<<(std::ostream &os, Storage<T> &elem)
+std::ostream &operator<<(std::ostream &os, Storage<T> elem)
 {
   os << elem.display();
   return os;
 }
-
 template<class T>
-T maximum(Storage<T> &elem)
+T maximum(Storage<T> elem)
 {
   T maxValue;
-  for(size_t looptimes = 0; looptimes < elem.size(); looptimes++)
+  for(size_t looptimes = 0; looptimes < elem.size; looptimes++)
   {
     if (elem.at(looptimes) < elem.at(looptimes++))
     {
@@ -85,43 +77,27 @@ T maximum(Storage<T> &elem)
 }
 
 template<class T>
-T searchElement(T searchVal, Storage<T> &elem)
+T searchElement(T searchVal, Storage<T> elem)
 {
   try
   {
-    if(elem.size() <= 0)
+    if(elem.size <= 0)
     {
       throw std::out_of_range("Out of range, it should be bigger than 0");
     }
-    else
+    for(size_t loop = 0; loop < elem.size; loop++)
     {
-      size_t first = 0;
-      size_t last = elem.size () - 1;
-      bool found = false;
-      while (!found && first < last && last < elem.size ())
-      {
-        size_t middle = (first + last) / 2;
-        if (elem.at (middle) == searchVal)
-        {
-          return true;
-        }
-        else if (elem.at (middle) > searchVal)
-        {
-          last = middle - 1;
-        }
-        else
-        {
-          first = middle + 1;
-        }
-      }
-      return false;
+       if(searchVal == elem[loop])
+       {
+         return true;
+       }
     }
+    return false;
   } catch (const char *error)
   {
      std::cout << "Error: " << error << std::endl;
   }
 }
-
 
 template<class T>
 void Storage<T>::display() const
@@ -133,18 +109,19 @@ void Storage<T>::display() const
     std::cout << array[loop] << ", ";
   }
 }
-
 int main()
 {
     int size;
-    try{
+    try
+    {
       std::cout << "Please input the size of the array: ";
       std::cin >> size;
       if (size <= 0)
       {
         throw StorageException();
       }
-    } catch (const char *error){
+    } catch (const char *error)
+    {
       std::cout << "Error: " << error << std::endl;
     }
     //Construct two object using the size as parameter to the array size
