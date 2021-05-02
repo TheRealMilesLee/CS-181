f770d156b84ed2e05c3369aaf1d526964b0a7e3f
Serendipity <hl3265@truman.edu>
2021-05-01 23:29:10 -0500

    。

--- Lab5-TemplateException/cmake-build-debug/CMakeFiles/Lab5_TemplateException.dir/cmake_clean.cmake
+++ Lab5-TemplateException/cmake-build-debug/CMakeFiles/Lab5_TemplateException.dir/cmake_clean.cmake
@@ -1,7 +1,9 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/Lab5_TemplateException.dir/StorageContainer.cpp.o"
-  "Lab5_TemplateException"
+  "CMakeFiles/Lab5_TemplateException.dir/StorageContainer.cpp.obj"
+  "Lab5_TemplateException.exe"
+  "Lab5_TemplateException.exe.manifest"
   "Lab5_TemplateException.pdb"
+  "libLab5_TemplateException.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
