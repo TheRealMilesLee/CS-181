96ea1755cad780f34f8f49e3c3c131c6b9302dc8
Serendipity <1789216340lhy@gmail.com>
2021-05-03 16:41:45 -0500

    update

--- Code Template/Copy constructor array.cpp
+++ Code Template/Copy constructor array.cpp
@@ -10,33 +10,18 @@ private:
   T *array;
   int size;
 public:
+//Constructor
   Storage(int length);
+//Copy constructor
   Storage(const Storage &elem);
   ~Storage();
-  int getSize()const
-  {return size;}
   T &operator[](const int &index);
-  template<class CT>
-  friend std::ostream &operator<<(std::ostream &stream_insertion,  Storage<CT> &obj);
 };
-template<class CT>
-std::ostream &operator<<(std::ostream &stream_insertion,  Storage<CT> &obj)
-{
-  for (size_t i = 0; i < obj.size; i++)
-  {
-     stream_insertion << obj[i];
-    if (i == obj.size - 1)
-    {
-      continue;
-    }
-    stream_insertion << ", ";
-  }
-  return stream_insertion;
-}
 template<class T>
 Storage<T>::Storage(int length)
 {
   size = length;
+  array = new T [size];
 }
 template <class T>
 T &Storage<T>::operator[](const int &index)
@@ -48,39 +33,16 @@ Storage<T>::Storage(const Storage &elem)
 {
   cout << "Copy constructor is called!";
   size = elem.size;
-  // allocate memory
   array = new T [size];
   for(int count = 0; count < size; count++)
   {
-    *array[count] = *elem.array[count];
-  }
-  
-  for (int i = 0; i < size; i++)
-  {
-    cout << elem[i] << endl;
+    *(array + count) = *(elem.array+count);
   }
 }
-
 template<class T>
 Storage<T>::~Storage()
 {
   // release all the allocated memory
   delete[] array;
   std::cout << "Deleting all the array elements ..." << std::endl;
-}
-
-int main()
-{
-  Storage<int> obj1(4);
-  obj1[0] =1;
-  obj1[1] =2;
-  obj1[2] =3;
-  obj1[3] =4;
-  cout << obj1 << endl;
-/*
-  Storage<int> *obj2;
-  obj2 = new Storage<int>(4);
-  obj1 = obj2; 
-*/
-  return 0;
-}
+};
