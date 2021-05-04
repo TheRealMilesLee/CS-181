25bbfd3392d2b3f577959cd4ca934cdf0523b266
Serendipity <1789216340lhy@gmail.com>
2021-05-03 22:49:44 -0500

    .

--- Code Template/Chapter 17<function objects, lambda, map, set, stl>/STL Array and iterator.cpp
+++ Code Template/Chapter 17<function objects, lambda, map, set, stl>/STL Array and iterator.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main()
 {
 //initialize
-array<int, 5> numbers{1, 2, 3, 4, 5};
-array<string, 4> names{"Jamie", "Ashley","Doug", "Claire"};
+array<int, 5> numbers = {1, 2, 3, 4, 5};
+array<string, 4> names = {"Jamie", "Ashley","Doug", "Claire"};
 
 //iterator
 array<string, 3>::iterator it;
